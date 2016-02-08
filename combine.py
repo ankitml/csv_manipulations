6ec35a9abaffe4e0c1b466151a0c13a7f709470e
Ankit <ankit@upgrad.com>
2016-02-08 14:43:16 +0530

    added acceptable_keys as kwarg into read data

--- combine.py
+++ combine.py
@@ -1,6 +1,6 @@
 from true_type import get_type
 
-def read_multiple_csv(files, key_header, rows=None):
+def read_multiple_csv(files, key_header, acceptable_keys=None):
     """
     takes a list of filesnames and a key_header string to merge the files. 
     Can merge if there are different columns in the files, different records in
@@ -13,32 +13,35 @@ def read_multiple_csv(files, key_header, rows=None):
     data = {}
     # files = ['assignments.csv', 'discuss.csv', 'questions.csv']
     # key_header = 'email'
+    # acceprable keys are list of keys allowed to go into final data
+    # if not given all will be accepted, this list can come out of 
+    # intersection or other set methods
     header_line = []
+    import pdb
+    pdb.set_trace()
 
     for filename in files:
-        file_generator = open(filename)
-        headers = file_generator.next().split(',')
-        headers = [h.strip() for h in headers]
-        header_line.extend(headers)
+        with open(filename, 'r+') as fo:
+            headers = fo.readline().split(',')
+            headers = [h.strip() for h in headers]
+            header_line.extend(headers)
 
-        for line in file_generator:
-            splits = line.split(',')
-            inner_dict = {}
-            for k,header in enumerate(headers):
-                #assumes header is a string. probably it is
-                if header is not '':
-                    inner_dict[header] = int(splits[k]) if get_type(splits[k]) is int else splits[k].strip()
-            data_key = inner_dict.pop(key_header)
-
-
-            try:
-                z = data[data_key].copy()
-            except KeyError:
-                z = {}
-            z.update(inner_dict)
-            data[data_key] = z
+            for line in fo:
+                splits = line.split(',')
+                inner_dict = {}
+                for k,header in enumerate(headers):
+                    #assumes header is a string. probably it is
+                    if header is not '':
+                        inner_dict[header] = int(splits[k]) if get_type(splits[k]) is int else splits[k].strip()
+                data_key = inner_dict.pop(key_header)
+                if data_key in acceptable_keys:
+                    try:
+                        z = data[data_key].copy()
+                    except KeyError:
+                        z = {}
+                    z.update(inner_dict)
+                    data[data_key] = z
             data['headers'] = list(set(header_line))
-        file_generator.close()
     return data
             
 def write_combined_csv(data, file_name='combined.csv'):
